0a224f9af084242450c07fa6a52ac2a5d7688a8d
minha <etchcure@gmail.com>
2021-09-01 23:10:10 +0900

    [10강] 스타일링 하기

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,5 +1,4 @@
 import User from "../models/User";
-import Video from "../models/Video";
 import fetch from "node-fetch";
 import bcrypt from "bcrypt";
 
@@ -216,7 +215,14 @@ export const postChangePassword = async (req, res) => {
 
 export const see = async (req, res) => {
   const { id } = req.params;
-  const user = await User.findById(id).populate("videos");
+  // populate 두번해서 video랑 owner가지고 오기
+  const user = await User.findById(id).populate({
+    path: "videos",
+    populate: {
+      path: "owner",
+      model: "User",
+    },
+  });
   if (!user) {
     return res.status(404).render("404", { pageTitle: "User not found." });
   }
